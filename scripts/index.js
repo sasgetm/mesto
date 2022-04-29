7a7ca0d6ff8c697472c49ebed2ead3eab36af494
Alexander Getmanets <sasgetm@yandex.ru>
2022-04-30 00:55:43 +0700

    Внести правки после второй проверки

--- scripts/index.js
+++ scripts/index.js
@@ -3,8 +3,8 @@ let profileEdit = document.querySelector('.profile__edit');
 let popupClose = document.querySelector('.popup__close');
 let nameElement = document.querySelector('.profile__name');
 let roleElement = document.querySelector('.profile__role');
-let nameInput = document.querySelector('.popup__input_name');
-let roleInput = document.querySelector('.popup__input_role');
+let nameInput = document.querySelector('.popup__input_type_name');
+let roleInput = document.querySelector('.popup__input_type_role');
 let formElement = document.querySelector('.popup__form');
 
 function showPopup() {
